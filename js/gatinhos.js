34f47a2da338b13ed361b23c1be59fec347bd2a3
Lucassimaog6 <22101918@aluno.cotemig.com.br>
2022-09-03 12:45:00 -0300

    make "copy" and "download" buttons

--- js/gatinhos.js
+++ js/gatinhos.js
@@ -1,7 +1,26 @@
 const img = document.querySelector("#img");
-const URL = "https://api.thecatapi.com/v1/images/search";
-document.querySelector("#btn").addEventListener("click", () => {
-	fetch(URL).then((resp) => resp.json()).then(function (data) {
-		img.innerHTML = `<img src="${data[0].url}"/>`
+var img_url = ""
+
+function gerar() {
+	fetch("https://api.thecatapi.com/v1/images/search").then((resp) => resp.json()).then(function (data) {
+		img.innerHTML = `<img download src="${data[0].url}"/>`;
+		img_url = data[0].url;
 	})
-})
+}
+function copiar() {
+	navigator.clipboard.writeText(img_url)
+}
+
+function download() {
+	if (img_url != "") {
+		fetch(img_url, { mode: "no-cors" }).then(resp => resp.blob()).then(blob => {
+			let blob_url = window.URL.createObjectURL(blob);
+			let a = document.createElement("a");
+			a.download = img_url.replace(/^.*[\\\/]/, '');
+			a.href = blob_url;
+			document.body.appendChild(a);
+			a.click();
+			a.remove();
+		})
+	}
+}
