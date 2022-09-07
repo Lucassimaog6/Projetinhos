fee7310bee1063e4663f49171a845e480b67481f
Lucassimaog6 <22101918@aluno.cotemig.com.br>
2022-09-06 21:49:57 -0300

    test

--- js/gatinhos.js
+++ js/gatinhos.js
@@ -1,5 +1,5 @@
-const download_btn = document.querySelector("#download")
 const img = document.querySelector("#img");
+
 function gerar() {
 	fetch("https://api.thecatapi.com/v1/images/search").then((resp) => resp.json()).then(function (data) {
 		img.innerHTML = `<img src="${data[0].url}"/>`;
